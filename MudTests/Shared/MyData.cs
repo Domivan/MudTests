b463d87b4bf44f66efebe0003b23964964188056
Иван <domivan@mail.ru>
2024-02-26 20:53:24 +0300

    update

---
+++ MudTests/Shared/MyData.cs
@@ -0,0 +1,10 @@
+﻿namespace MudTests.Shared
+{
+    public delegate Task Update();
+    public class MyData
+    {
+        public string Category { get; set; }
+        public int NetProfit { get; set; }
+        public int Revenue { get; set; }
+    }
+}
